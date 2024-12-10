1c755c4d574d46c06397bae06e6ba1e8dc314c20
aurum77 <58880138+aurum77@users.noreply.github.com>
2024-12-10 09:04:41 +0300

    feat: implement GetProductsEndpoint and GetProductsHandler

---
+++ Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,21 @@
+﻿using FastEndpoints;
+
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndpoint(ISender sender) : EndpointWithoutRequest<GetProductsResponse>
+{
+    public override void Configure()
+    {
+        Get("/products");
+        AllowAnonymous();
+    }
+
+    public override Task HandleAsync(CancellationToken ct)
+    {
+        var result = sender.Send(new GetProductsQuery());
+        var response = result.Result.Adapt<GetProductsResponse>();
+        return SendAsync(response, StatusCodes.Status302Found);
+    }
+}
