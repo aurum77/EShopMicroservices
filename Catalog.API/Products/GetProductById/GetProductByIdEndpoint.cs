e80a7dcd7830db697ddeb511d28f0ccc4f681fef
aurum77 <58880138+aurum77@users.noreply.github.com>
2024-12-10 17:47:13 +0300

    feat: implement GetProductById Endpoint and Handler

---
+++ Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using FastEndpoints;
+
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdRequest(Guid Id);
+
+public record GetProductByIdResponse(Product Product);
+
+public class GetProductByIdEndpoint(ISender sender)
+    : Endpoint<GetProductByIdRequest, GetProductByIdResponse>
+{
+    public override void Configure()
+    {
+        Get("/products/{id}");
+        AllowAnonymous();
+        Description(b =>
+            b.WithName("GetProductById")
+                .Produces<GetProductByIdResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Product by Id")
+                .WithDescription("Get Product by Id")
+        );
+    }
+
+    public override Task HandleAsync(GetProductByIdRequest req, CancellationToken ct)
+    {
+        var query = new GetProductByIdQuery(req.Id);
+        var result = sender.Send(query).Result;
+        var response = result.Adapt<GetProductByIdResponse>();
+        return SendAsync(response, StatusCodes.Status200OK);
+    }
+}
