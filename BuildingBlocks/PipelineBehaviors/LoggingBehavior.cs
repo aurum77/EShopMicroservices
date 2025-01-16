93e3fa38a618a33388d532ce54633359fd26629c
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-01-16 11:32:27 +0300

    feata: add LoggingBehavior pipeline behavior

---
+++ BuildingBlocks/PipelineBehaviors/LoggingBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using BuildingBlocks.CQRS;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace BuildingBlocks.PipelineBehaviors;
+
+public class LoggingBehavior<TRequest, TResponse>(
+    ILogger<LoggingBehavior<TRequest, TResponse>> logger
+) : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : ICommand<TResponse>
+{
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken
+    )
+    {
+        logger.LogInformation(
+            "[START] handling Request {Request} - Response {Response} - RequestData {RequestData}",
+            typeof(TRequest).Name,
+            typeof(TResponse).Name,
+            request
+        );
+
+        var timer = new Stopwatch();
+        timer.Start();
+
+        var response = await next();
+
+        timer.Stop();
+
+        var timeElapsed = timer.Elapsed;
+
+        if (timeElapsed.Seconds > 3)
+        {
+            logger.LogWarning(
+                "[PERFORMANCE] The request {Request} took {timeElapsed}",
+                typeof(TRequest).Name,
+                timeElapsed
+            );
+        }
+
+        logger.LogInformation(
+            "[END] handled Request {Request} with Response {Response} - RequestData {RequestData}",
+            typeof(TRequest).Name,
+            typeof(TResponse).Name
+        );
+
+        return await next();
+    }
+}
