ab3aefb511490b0683d97eb4fab10eb021925d23
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-02-03 00:06:58 +0300

    feat: implement GetBasket with stub

---
+++ Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -0,0 +1,34 @@
+﻿using Basket.API.Models;
+using FastEndpoints;
+using Mapster;
+using MediatR;
+
+namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketRequest(string Username);
+
+public record GetBasketResponse(ShoppingCart ShoppingCart);
+
+public class GetBasketEndpoint(ISender sender) : Endpoint<GetBasketRequest, GetBasketResponse>
+{
+    public override void Configure()
+    {
+        Get("/basket/{username}");
+        AllowAnonymous();
+        Description(b =>
+            b.WithName("GetBasket")
+                .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Get Basket")
+                .WithDescription("Get Basket")
+        );
+    }
+
+    public override async Task HandleAsync(GetBasketRequest req, CancellationToken ct)
+    {
+        var query = req.Adapt<GetBasketQuery>();
+        var result = sender.Send(query).Result;
+        var response = result.Adapt<GetBasketResponse>();
+        await SendAsync(response, StatusCodes.Status200OK, ct);
+    }
+}
---
+++ Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,22 @@
+﻿using Basket.API.Models;
+using BuildingBlocks.CQRS;
+
+namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string Username) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingCart ShoppingCart);
+
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    public async Task<GetBasketResult> Handle(
+        GetBasketQuery query,
+        CancellationToken cancellationToken
+    )
+    {
+        // TODO: get basket from db
+        // var basket = await _repository.GetBasket(query.Username);
+
+        return new GetBasketResult(new ShoppingCart("au"));
+    }
+}
