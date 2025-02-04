df6d538f05903e1ca574fdf37dcc6dad76fff412
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-02-04 18:21:33 +0300

    setup necessary middleware for MediatR, FastEndpoints, Marten

--- Basket.API/Program.cs
+++ Basket.API/Program.cs
@@ -1,9 +1,36 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using BuildingBlocks.Exceptions.Handler;
+using BuildingBlocks.PipelineBehaviors;
+using FastEndpoints;
+using HealthChecks.UI.Client;
+
+var assembly = typeof(Program).Assembly;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the DI container
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddHealthChecks();
+builder.Services.AddFastEndpoints();
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("PostgreSQL")!);
+}).UseLightweightSessions();
+
+builder.Services.AddLogging();
+builder.Services.AddValidatorsFromAssembly(assembly);
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
-
-//Configure the HTTP request pipeline
+app.UseHealthChecks(
+    "/health",
+    new HealthCheckOptions { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse }
+);
+app.UseFastEndpoints();
+app.UseExceptionHandler(options => { });
 
 app.Run();
