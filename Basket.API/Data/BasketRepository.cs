891d2c53e690622567ec85ec976d91a5a0807114
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-02-05 10:49:48 +0300

    implement BasketRepository

---
+++ Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,30 @@
+﻿using Basket.API.Exceptions;
+using Basket.API.Models;
+
+namespace Basket.API.Data;
+
+public class BasketRepository(IDocumentSession session) : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasket(string username)
+    {
+        var basket = await session.LoadAsync<ShoppingCart>(username);
+
+        return basket is not null ? basket : throw new BasketNotFoundException(username);
+    }
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart basket)
+    {
+        session.Store(basket);
+        await session.SaveChangesAsync();
+
+        return basket;
+    }
+
+    public async Task<bool> DeleteBasket(string username)
+    {
+        session.Delete<ShoppingCart>(username);
+        await session.SaveChangesAsync();
+
+        return true;
+    }
+}
---
+++ Basket.API/Data/IBasketRepository.cs
@@ -0,0 +1,10 @@
+﻿using Basket.API.Models;
+
+namespace Basket.API.Data;
+
+public interface IBasketRepository
+{
+    Task<ShoppingCart> GetBasket(string username);
+    Task<ShoppingCart> StoreBasket(ShoppingCart basket);
+    Task<bool> DeleteBasket(string username);
+}
