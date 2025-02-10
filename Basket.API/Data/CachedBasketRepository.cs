0e9fc13549015a929bb1e0fceb097c4b0aad673f
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-02-10 18:37:47 +0300

    create CachedBasketRepository

---
+++ Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using Basket.API.Models;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Basket.API.Data;
+
+public class CachedBasketRepository(IBasketRepository repository, IDistributedCache cache) : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasket(string username, CancellationToken cancellationToken)
+    {
+        var cachedBasket = await cache.GetStringAsync(username, cancellationToken);
+
+        if (!string.IsNullOrEmpty(cachedBasket)) 
+        { 
+            JsonSerializer.Deserialize<ShoppingCart>(cachedBasket);
+        }
+
+        var basket = await repository.GetBasket(username, cancellationToken);
+        await cache.SetStringAsync(username, JsonSerializer.Serialize(basket), cancellationToken);
+
+        return cachedBasket ?? ;
+    }
+
+    public async Task<ShoppingCart> StoreBasket(
+        ShoppingCart basket,
+        CancellationToken cancellationToken
+    )
+    {
+        return await repository.StoreBasket(basket, cancellationToken);
+    }
+
+    public async Task<bool> DeleteBasket(string username, CancellationToken cancellationToken)
+    {
+        return await repository.DeleteBasket(username, cancellationToken);
+    }
+}
