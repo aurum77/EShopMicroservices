d1db36f487fc30eca454942e7189c6cc99f97def
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-02-12 18:10:41 +0300

    add sample data to DiscountContext

--- Discount.Grpc/Data/DiscountContext.cs
+++ Discount.Grpc/Data/DiscountContext.cs
@@ -8,5 +8,27 @@ public class DiscountContext : DbContext
     public DiscountContext(DbContextOptions options)
         : base(options) { }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Coupon>()
+            .HasData(
+                new Coupon
+                {
+                    Id = 1,
+                    ProductName = "IPhone 14",
+                    Description = "Student discount",
+                    Amount = 150,
+                },
+                new Coupon
+                {
+                    Id = 2,
+                    ProductName = "MacBook Air M3",
+                    Description = "Student discount",
+                    Amount = 100,
+                }
+            );
+    }
+
     public DbSet<Coupon> Coupons { get; set; } = default!;
 }
