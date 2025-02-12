dec5e10bdc4d7ce9d86ef52265759cbccbcd0817
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-02-12 17:12:38 +0300

    setup EntityFrameworkCore with SQLite

--- Discount.Grpc/Program.cs
+++ Discount.Grpc/Program.cs
@@ -1,9 +1,15 @@
+using Discount.Grpc.Data;
 using Discount.Grpc.Servicescount;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<DiscountContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("SQLite"));
+});
 
 var app = builder.Build();
 
