99896502d82835b315bb907af3dec164cc501817
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-02-17 00:13:49 +0300

    feat: setup Discount.Grpc inside docker

--- Basket.API/Program.cs
+++ Basket.API/Program.cs
@@ -41,6 +41,16 @@ builder.Services.AddStackExchangeRedisCache(options =>
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
 {
     options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        // This should not be used in a Production environment due to security risks !!!
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+
+    return handler;
 });
 
 var app = builder.Build();
