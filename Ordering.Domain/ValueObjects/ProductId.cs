6891942efa9924950052419284ef1eb94c93f528
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-02-27 14:47:18 +0300

    fix: fix ProductId.Of not being static

--- Ordering.Domain/ValueObjects/ProductId.cs
+++ Ordering.Domain/ValueObjects/ProductId.cs
@@ -5,7 +5,7 @@ public record ProductId
     public Guid Value { get; }
     private ProductId(Guid value) => Value = value;
 
-    public ProductId Of(Guid value)
+    public static ProductId Of(Guid value)
     {
         ArgumentNullException.ThrowIfNull(value);
 
