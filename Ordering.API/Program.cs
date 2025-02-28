64bfa305dffa92ce5e7dfee7b586464fa13544b7
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-02-28 17:59:53 +0300

    create Infrastructure DI Module

--- Ordering.API/Program.cs
+++ Ordering.API/Program.cs
@@ -1,9 +1,12 @@
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to DI container
+builder.Services
+    .AddInfrastructureServices();
 
 var app = builder.Build();
 // Configure the request pipeline
-
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
---
+++ Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
+
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        var connectionString = configuration.GetConnectionString("SQLServer");
+
+        services.AddDbContext<ApplicationDbContext>(
+            (sp, options) =>
+            {
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseSqlServer(connectionString);
+            }
+        );
+
+        return services;
+    }
+}
