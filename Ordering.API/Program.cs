4e51fbaddfabd18f7a267eaf107047a273901709
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-06-09 13:17:23 +0300

    fix: add missing configuration when calling AddInfrastructureServices in Program.cs

--- Ordering.API/Program.cs
+++ Ordering.API/Program.cs
@@ -1,10 +1,11 @@
 using Ordering.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
 // Add services to DI container
 builder.Services
-    .AddInfrastructureServices();
+    .AddInfrastructureServices(configuration);
 
 var app = builder.Build();
 // Configure the request pipeline
