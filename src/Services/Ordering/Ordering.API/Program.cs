679ccdcc02f815e06c25e75f1eb72b7ee75602c2
aurum77 <58880138+aurum77@users.noreply.github.com>
2025-10-10 22:05:39 +0300

    add db init in program.cs

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,13 +1,20 @@
+using Ordering.API;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
 // Add services to DI container
-builder.Services
-    .AddInfrastructureServices(configuration);
+builder.Services.AddInfrastructureServices(configuration);
 
 var app = builder.Build();
+
 // Configure the request pipeline
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitializeDatabaseAsync();
+}
 
 app.Run();
---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Mapster;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure.Data.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static async Task InitializeDatabaseAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+        await Seed
+    }
+
+    private static async async SeedAsync(ApplicationDbContext context)
+    {
+        await SeedCustomer(context):
+        await SeedProductAsync(context):
+        await SeedOrderAndItemsAsync(context):
+    }
+
+    private static async Task SeedCustomerAsync(ApplicationDbContext context)
+    {
+        if (!await context.Customers.AnyAsync())
+        {
+            await context.Customers.AddRangeAsync(InitialData.Customer)
+        }
+    }
+}
